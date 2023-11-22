8aa27a6aa1abb0e8104326fe427b355bea07b366
Laurenande <mine-vip@yandex.ru>
2023-11-22 12:15:40 +0300

    Redesign AgentFormIP, AgentFormUr, BidCell, CustomTextField. Delete Tariff View

---
+++ TehnoleasingCabinet/Controller/Policy/PolicyIpAndFisVC.swift
@@ -0,0 +1,29 @@
+//
+//  PolicyIpAndFisVC.swift
+//  TehnoleasingCabinet
+//
+//  Created by Егор Куракин on 21.11.2023.
+//
+
+import UIKit
+
+class PolicyIpAndFisVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
