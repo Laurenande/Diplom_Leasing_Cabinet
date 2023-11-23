8197a921c14ea09c66b1a58bf9d6d906fa6db214
Laurenande <mine-vip@yandex.ru>
2023-11-23 17:27:03 +0300

    Add Settings view

---
+++ TehnoleasingCabinet/View/SwitchTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SwitchTableViewCell.swift
+//  TehnoleasingCabinet
+//
+//  Created by Егор Куракин on 23.11.2023.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
